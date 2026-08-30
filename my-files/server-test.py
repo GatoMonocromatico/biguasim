407294ea58494662a844ef4ab7ada194dce3579b
ivo <ivoarpino2@gmail.com>
2026-08-30 11:52:01 -0300

    adding sitl support for served worlds

--- my-files/server-test.py
+++ my-files/server-test.py
@@ -6,10 +6,10 @@ with RemoteWorld(address="100.119.211.18", port=8770, client_id="pilot", scenari
     world.watch_state()
 
     landed = world.spawn_agent(
-        "uav67", "DjiMatrice",
+        "uav1", "DjiMatrice",
         location=(0.0, 0.0, 5.0),
         sensors=[{"sensor_type": "DynamicsSensor", "socket": "IMUSocket",
                   "configuration": {"UseCOM": True, "UseRPY": False}}])
 
-    world.set_control("uav20", [330.0] * 4)
+    world.set_control("uav1", [330.0] * 4)
     state = world.wait_for_tick(landed + 20)
